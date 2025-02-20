7b98c573eca43e7bd1c7f55f03c039a04ed45356
AutomaticPotato <AutomaticPotato@pm.me>
2025-02-20 14:23:57 +0100

    Added weather description

--- components/WeatherWidget.vue
+++ components/WeatherWidget.vue
@@ -1,16 +1,17 @@
 <script setup lang="ts">
 import { WMOCodes } from '~/constants/WMOCodes';
 import type { ForecastResponse } from '~/types/responses/ForecastResponse';
+import type { WeatherCode } from '~/types/shared/WeatherCode';
 
 defineProps<{
   weatherData: ForecastResponse
 }>()
 
-function getWeatherIcon(weatherCode: number): string {
+function getWeatherInfo(weatherCode: number): WeatherCode {
   try {
-    return WMOCodes[weatherCode].day.image
+    return WMOCodes[weatherCode]
   } catch {
-    return WMOCodes[0].day.image
+    return WMOCodes[0]
   }
 }
 
@@ -18,18 +19,23 @@ function getWeatherIcon(weatherCode: number): string {
 
 <template>
   <CardContainer class="flex flex-col relative w-full lg:w-auto mx-5">
-    <div class="flex justify-between items-center group">
+    <div class="flex justify-between items-center group border-b border-white/10">
       <div class="flex flex-col">
         <span>{{ weatherData?.locationName }}</span>
         <span class="text-3xl">{{ weatherData?.current.temperature_2m }} &#8451;</span>
       </div>
-      <img class="transition-all group-hover:scale-110 duration-300 ease-out-overshoot"
-        :src="getWeatherIcon(weatherData?.current.weather_code!)" />
+      <div class="flex flex-col">
+        <img class="transition-all group-hover:scale-110 duration-300 ease-out-overshoot"
+          :src="getWeatherInfo(weatherData?.current.weather_code!).day.image" />
+        <span class="-translate-y-5">{{
+          getWeatherInfo(weatherData.current.weather_code!).day.description
+          }}</span>
+      </div>
     </div>
     <div class="grid grid-cols-1 lg:grid-cols-7 gap-3 mt-5">
       <DailyWeather v-for="(day, index) in weatherData?.daily.time" :key="day"
         :class="{ 'lg:border-r lg:border-b-0 border-b border-white/10': index < weatherData?.daily.time.length! - 1 }"
-        :weather-icon-url="getWeatherIcon(weatherData?.daily.weather_code[index]!)"
+        :weather-icon-url="getWeatherInfo(weatherData?.daily.weather_code[index]!).day.image"
         :temperatureMin="weatherData?.daily.temperature_2m_min[index]!"
         :temperatureMax="weatherData?.daily.temperature_2m_max[index]!" :date="weatherData?.daily.time[index]!" />
     </div>
--- pages/index.vue
+++ pages/index.vue
@@ -8,15 +8,15 @@ const latitude = computed(() => 53.21064892641366)
 const longitude = computed(() => 6.584382254116292)
 
 const { data: weatherData } = await useFetch<ForecastResponse>("/api/forecast", {
-    query: {
-        latitude,
-        longitude,
-    }
+  query: {
+    latitude,
+    longitude,
+  }
 })
 
 </script>
 <template>
-    <div class="bg-(image:--gradient-pink) min-h-[100dvh] w-full flex justify-center items-center py-5">
-        <WeatherWidget :weatherData="weatherData!" />
-    </div>
+  <div class="bg-(image:--gradient-pink) min-h-[100dvh] w-full flex justify-center items-center py-5">
+    <WeatherWidget :weatherData="weatherData!" />
+  </div>
 </template>
